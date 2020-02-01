29001832a1e7969ff6fe85accb882b3c93076640
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-02-01 11:38:27 -0500

    Sign-In-Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Switch, Route} from "react-router-dom";
 
 import './App.css';
+import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component"
 
 function App() {
   return (
+
     <div className="App">
-      
+     <Route path="/signin" component={SignInSignUpPage}></Route>
     </div>
+
   );
 }
 
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./sign-in-sign-up.styles.scss";
+import {SignIn} from "../../components/sing-in/sign-in.component"
+
+const SignInSignUpPage=()=>
+(
+    <div className='sign-in-and-sign-up'><SignIn></SignIn></div>
+)
+
+export default SignInSignUpPage;
