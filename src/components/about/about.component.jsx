2c8cf0b27791a8ae9817a2488756f4fe8884d5a6
CristianCgu <46873555+CristianCgu@users.noreply.github.com>
2020-02-01 17:51:58 -0500

    about

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -4,7 +4,9 @@ const About = ()=>(
     <div>
 <h1>FeedMe is a service that connects food shelters to restaurants, caterers and grocery stores
      in order to minimize food waste. The initiative started in 2020, when local restaurants started 
-     considering an alternative to food waste management. 
+     considering an alternative to food waste management. The founders of the web service, graduate
+     McGill students, are planning on nationalizing the service in order to create a large-scale impact
+     on our patterns of food management, while giving back to those most in need. 
       </h1>
     </div>
 );
