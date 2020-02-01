92b836aa9d9c9e1e027f46f79625cd4f43f41ca7
CristianCgu <46873555+CristianCgu@users.noreply.github.com>
2020-02-01 17:54:07 -0500

    about

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = ()=>(
     <div>
-<h1>FeedMe is a service that connects food shelters to restaurants, caterers and grocery stores
+<h1>FeedGreen is a service that connects food shelters to restaurants, caterers and grocery stores
      in order to minimize food waste. The initiative started in 2020, when local restaurants started 
      considering an alternative to food waste management. The founders of the web service, graduate
      McGill students, are planning on nationalizing the service in order to create a large-scale impact
