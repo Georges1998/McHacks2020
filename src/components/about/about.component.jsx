e4769f14c9d1994ff3f3d69cb39d8641714af22f
CristianCgu <46873555+CristianCgu@users.noreply.github.com>
2020-02-01 19:06:43 -0500

    commit

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
+import "./about.styles.scss";
 const About = ()=>(
-    <div>
-<h1>About</h1><p>FeedGreen started in 2020, when local restaurants started considering an alternative to food 
+    <div className="George">
+<h1>About</h1><span id="ss"><p>FeedGreen started in 2020, when local restaurants started considering an alternative to food 
     waste management. The founders of the web service, graduate McGill students, are planning on 
     nationalizing the service in order to create a large-scale impact on our patterns of food 
     management, while giving back to those most in need. 
       </p>
+      </span>
     </div>
 );
 
